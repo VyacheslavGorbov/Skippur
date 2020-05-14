7cdda21b04d886202ad3cfa45826d56afd0dc62b
Vyacheslav Gorbov <1481679@edu.vaniercollege.qc.ca>
2020-05-13 23:49:03 -0400

    Readded the register.php file into the customer folder

---
+++ app/views/customer/register.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+	<title>Customer Registration</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+
+<body>
+	<div align="center">
+		Enter your registration information below
+		<form action='/customer/index' method="post" class="container">
+			<label>Name:</label><input type='text' name='customer_name' /><br />
+			<label>Email:</label><input type='text' name='customer_email' /><br />
+			<input type="submit" name="action" value="Register" class="btn btn-primary">
+		</form>
+	</div>
+
+
+</body>
+
+</html>
